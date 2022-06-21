c5f7404c495ba855894e49bcf0581566e060cce0
Pranit <pdghorpade40@gmail.com>
2022-06-21 10:18:16 +0530

    Advance Program

---
+++ AdvanceCalculator.java
@@ -0,0 +1,16 @@
+package gen_Question;
+
+public class AdvanceCalculator {
+	public static void main(String[] args) {
+		int a=90;
+		int b=30;
+		
+		int exp=(a/b)*(a+b);
+		int exp1=(a/b)*(a-b);
+		
+		System.out.println("Expression 1:-"+exp);
+		System.out.println("Expresion 2:-"+exp1);
+
+		
+	}
+}
