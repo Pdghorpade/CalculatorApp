baa7ace024bd77a2d68fd8fb8849c8f019b1f1ac
Pranit <pdghorpade40@gmail.com>
2022-06-21 09:42:39 +0530

    Initial File

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+package gen_Question;
+
+public class Calculator {
+public static void main(String[] args) {
+	int a=20;
+	int b=30;
+	
+	int c=a+b;
+	
+	int d=a*b;
+	System.out.println("Addition:-"+c);
+	System.out.println("Multiplication:-"+d);
+			
+}
+}
